d78640c307da1d47c3d6443ea60cc251dc5546fd
Nanech <xisaby@gmail.com>
2023-03-11 18:15:32 +0300

    add some new layouts

---
+++ app/src/main/java/com/example/myfirstapllication/buy_tickets.kt
@@ -0,0 +1,11 @@
+package com.example.myfirstapllication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class buy_tickets : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buy_tickets)
+    }
+}
---
+++ app/src/main/java/com/example/myfirstapllication/list_of_dates.kt
@@ -0,0 +1,11 @@
+package com.example.myfirstapllication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class list_of_dates : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_of_dates)
+    }
+}
